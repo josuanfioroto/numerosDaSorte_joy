afa2ec5c85a0d898f729599eb533e67949e75e3a
josuan_fioroto <142553191+josuanfioroto@users.noreply.github.com>
2024-04-16 19:05:53 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,9 @@
-// Array de números da sorte
+
 let numerosDaSorte = [37, 14, 26, 5, 94, 87];
 
-// Função para avaliar e imprimir frases com base nos números
 function avaliarNumerosDaSorte(numeros) {
-    // Loop através de cada número no array
     for (let i = 0; i < numeros.length; i++) {
         let numero = numeros[i]; // Pegar o número atual
-        
-        // Verificar as condições e imprimir a frase apropriada
         if (numero % 2 === 0 && numero < 50) {
             console.log(numero + ' é par e menor que 50');
         } else if (numero < 50) {
@@ -17,6 +13,4 @@ function avaliarNumerosDaSorte(numeros) {
         }
     }
 }
-
-// Chamar a função com o array numerosDaSorte
 avaliarNumerosDaSorte(numerosDaSorte);
